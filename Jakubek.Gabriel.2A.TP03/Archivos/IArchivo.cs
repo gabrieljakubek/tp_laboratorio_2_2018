969249b0496b35dff400bdb54ade4ba3377e9f22
gabrieljakubek <gabrieljakubek@gmail.com>
2018-11-04 23:52:08 -0300

    Se inicio el TP03

---
+++ Jakubek.Gabriel.2A.TP03/Archivos/IArchivo.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public interface IArchivo<T>
+    {
+        /// <summary>
+        /// Metodo de interfaz para guardar
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        bool Guardar(string archivo, T datos);
+
+        /// <summary>
+        /// Metodo de interfaz para leer
+        /// </summary>
+        /// <param name="archivo"></param>
+        /// <param name="datos"></param>
+        /// <returns></returns>
+        bool Leer(string archivo, out T datos);
+    }
+}
---
+++ Jakubek.Gabriel.2A.TP03/Archivos/Texto.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Archivos
+{
+    public class Texto : IArchivo<string>
+    {
+        public bool Guardar(string archivo, string datos)
+        {
+            bool retorno = false;
+
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return retorno;
+        }
+
+        public bool Leer(string archivo, out string datos)
+        {
+            bool retorno = false;
+
+            datos = "";
+
+            try
+            {
+
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+
+            return retorno;
+        }
+    }
+}
